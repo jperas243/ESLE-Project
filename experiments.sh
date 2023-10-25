b77d238dc151b6c9f56537b8c20dbf4ac46ba794
Alexander Binett <alexanderbinett@Alexanders-MacBook-Pro.local>
2023-10-25 14:46:01 +0100

    added experiments to perform

---
+++ experiments.sh
@@ -0,0 +1,8 @@
+sh build_cluster_5.sh 4GB 15% 30% 3 512 #exp1
+sh build_cluster_5.sh 4GB 30% 15% 5 256 #exp5
+sh build_cluster_5.sh 8GB 15% 30% 5 256 #exp3
+sh build_cluster_5.sh 8GB 30% 15% 5 512 #exp7
+sh build_cluster_10.sh 4GB 15% 15% 5 512 #exp2
+sh build_cluster_10.sh 4GB 30% 30% 5 256 #exp6
+sh build_cluster_10.sh 8GB 15% 15% 3 256 #exp4
+sh build_cluster_10.sh 8GB 30% 30% 3 512 #exp8
